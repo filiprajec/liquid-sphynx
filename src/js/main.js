9526e83a7f1ded8fe50d2ebb34ae6b2a9c64b7e8
Filip Rajec <filiprajec@icloud.com>
2021-06-21 15:40:25 +1000

    added Vue

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,7 @@
 import "bootstrap";
+import Vue from "vue";
+import EnquiryForm from "./EnquiryForm.vue";
+
+new Vue({
+  render: (h) => h(EnquiryForm),
+}).$mount("#EnquiryForm");
--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,17 @@
 const path = require("path");
+const { VueLoaderPlugin } = require("vue-loader");
 
 module.exports = {
   entry: "./src/js/main.js",
   output: {
     path: path.resolve(__dirname, "build"),
     filename: "bundle.js",
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    },
+    extensions: ["*", ".js", ".vue", ".json"],
   },
   module: {
     rules: [
@@ -20,10 +27,23 @@ module.exports = {
         use: {
           loader: "babel-loader",
           options: {
-            presets: ["@babel/preset-env"],
+            presets: [
+            ["@babel/preset-env",
+              {
+                useBuiltIns: "usage",
+                corejs: 3,
+              }]
+            ],
           },
         },
       },
+      {
+        test: /\.vue$/,
+        loader: "vue-loader",
+      },
     ],
   },
+  plugins: [
+    new VueLoaderPlugin(),
+  ],
 };
